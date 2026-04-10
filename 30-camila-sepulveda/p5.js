95227df4bb3fb045135c964bb68ef4ab74dbd4de
camilasepulvedab <camila.sepulveda5@mail.udp.cl>
2026-04-10 11:07:23 -0400

    Update p5.js

--- 30-camila-sepulveda/p5.js
+++ 30-camila-sepulveda/p5.js
@@ -4,3 +4,7 @@
 (mousrX,mouseY);      ej: ellipse(mouseX,mouseY,100,100);
 
 para que hyaya una estela de la figura cuando se mueve con el mouse, el background debe estar arriba en setup, pero para que no deje estela y solo le mueva la figura se deja en draw
+
+##Crear propia variable. 
+
+  **let** para declarar variable, para variables dinámicas. Para dar espacio entre palabras, la segunda palabras se comienza con mayúscula para generar un espacio. La variable se pone arriba del todo, arriba del setup.
